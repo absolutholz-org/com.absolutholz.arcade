08bc9fdbbe70c8877bf7f98674ca980fd0418ee4
absolutholz <absolutholz@gmail.com>
2022-02-01 21:36:52 +0100

    Update global reset

--- src/Global.styled.tsx
+++ src/Global.styled.tsx
@@ -1,14 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
+// https://github.com/absolutholz/semantic-reboot
+
 export const GlobalStyle = createGlobalStyle`
     *, ::after, ::before {
         box-sizing: border-box;
     }
+    
+    @viewport {
+		width: device-width;
+	}
 
     :root {
         --accent: #006ecd;
-        --surface: #fffff7;
-        --on-surface: #111;
+        --on-accent: #fff;
+        --surface: #fffff8;
+        --on-surface: #11111a;
         
         @media (prefers-color-scheme: dark) {
             --accent: #007eff;
@@ -18,15 +25,27 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     html {
+        font: normal 1em / 1.42 system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif;
         height: 100%;
+        -webkit-text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+        -ms-overflow-style: scrollbar;
+        @if ($root-momentum-scrolling) {
+            -webkit-overflow-scrolling: touch;
+        }
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     }
 
     body {
-        min-height: 100%;
-
         accent-color: var(--accent);
         background: var(--surface);
         color: var(--on-surface);
+        margin: 0;
+        min-height: 100%;
+    }
+
+    [tabindex="-1"]:focus {
+        outline: none !important;
     }
 
     a {
@@ -42,6 +61,12 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     button {
-        all: unset;
+        background: none;
+        border: 0;
+        border-radius: 0;
+        color: inherit;
+        font: inherit;
+        margin: 0;
+        padding: 0;
     }
 `;
