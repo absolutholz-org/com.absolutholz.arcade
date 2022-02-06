33b9195268bbcf1a84a493015dac2b6443d93db7
absolutholz <absolutholz@gmail.com>
2022-02-06 10:58:40 +0100

    Update card with tag and props

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,8 @@
 import { ICardProps } from './ICard';
 import * as S from './Card.styled';
 
-export function Card ({ children, tag }: ICardProps): JSX.Element {
+export function Card ({ children, tag = 'div', ...props }: ICardProps): JSX.Element {
     return (
-        <S.Card as={ tag }>{ children }</S.Card>
+        <S.Card as={ tag } {...props}>{ children }</S.Card>
     );
 }
