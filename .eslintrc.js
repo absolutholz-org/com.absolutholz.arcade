4441db6ac3612e5bb3ef139f7fe6e8ebdbbe1333
absolutholz <absolutholz@gmail.com>
2022-02-10 21:34:27 +0100

    Add initial eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// https://blog.theodo.com/2019/08/empower-your-dev-environment-with-eslint-prettier-and-editorconfig-with-no-conflicts/
+
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+		'eslint:recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+		'prettier/@typescript-eslint',
+    ],
+    plugins: [
+		'prettier',
+	],
+    rules: {
+        'prettier/prettier': 'error',
+    },
+};
