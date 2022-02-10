1491c8dbede3b21802233c281b2e28317ea61bbd
absolutholz <absolutholz@gmail.com>
2022-02-10 22:03:54 +0100

    Update eslint prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
 // https://blog.theodo.com/2019/08/empower-your-dev-environment-with-eslint-prettier-and-editorconfig-with-no-conflicts/
+// https://glebbahmutov.com/blog/configure-prettier-in-vscode/
 
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
 		'eslint:recommended',
-        'prettier',
-        'plugin:prettier/recommended',
-		'prettier/@typescript-eslint',
-    ],
-    plugins: [
 		'prettier',
+		'plugin:prettier/recommended',
+		'prettier/@typescript-eslint',
 	],
-    rules: {
-        'prettier/prettier': 'error',
-    },
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+	},
 };
