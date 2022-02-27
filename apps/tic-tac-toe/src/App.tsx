e5977bd320f0f6283dd181b5c0144f0d4bdb4074
absolutholz <absolutholz@gmail.com>
2022-02-27 15:54:15 +0100

    Update tic tac toe with global styles

--- apps/tic-tac-toe/src/App.tsx
+++ apps/tic-tac-toe/src/App.tsx
@@ -1,16 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { PlayersProvider } from './context/Players';
 import { GamePage } from './pages/GamePage';
 import { HomePage } from './pages/HomePage';
 
+import * as S from '@arcade/common/Global.styled';
+
 export function App(): JSX.Element {
 	return (
 		<PlayersProvider>
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='game' element={<GamePage />} />
-			</Routes>
+			<S.GlobalStyle />
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='game' element={<GamePage />} />
+				</Routes>
+			</BrowserRouter>
 		</PlayersProvider>
 	);
 }
