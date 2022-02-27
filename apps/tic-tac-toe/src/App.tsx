854eb9193eecfc07805c9bf5d1821bc6f03d72d3
absolutholz <absolutholz@gmail.com>
2022-02-27 16:20:09 +0100

    Add global styles to tic tac toe

--- apps/tic-tac-toe/src/App.tsx
+++ apps/tic-tac-toe/src/App.tsx
@@ -4,12 +4,12 @@ import { PlayersProvider } from './context/Players';
 import { GamePage } from './pages/GamePage';
 import { HomePage } from './pages/HomePage';
 
-import * as S from '@arcade/common/Global.styled';
+import { GlobalStyle } from '@arcade/components';
 
 export function App(): JSX.Element {
 	return (
 		<PlayersProvider>
-			<S.GlobalStyle />
+			<GlobalStyle />
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<HomePage />} />
