6e63942aa3442b2a7116113aa159e8f09b59b635
absolutholz <absolutholz@gmail.com>
2022-02-27 10:33:35 +0100

    Move components to library package

--- libraries/components/src/components/Test/Test.tsx
+++
@@ -1,3 +0,0 @@
-export function Test(): JSX.Element {
-	return <div>Test</div>;
-}
--- src/components/Icon/IIcon.ts
+++
@@ -1,8 +0,0 @@
-import { FunctionComponent } from 'react';
-
-import { IconSize } from '../../enums/IconSize';
-
-export interface IIconProps {
-	children: FunctionComponent;
-	size?: IconSize;
-}
