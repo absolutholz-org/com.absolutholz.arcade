1bf3d0e45f7944bf21c5f8dd2cd2e0bfa7ebd4b4
absolutholz <absolutholz@gmail.com>
2022-03-30 09:04:27 +0200

    Update landing pages

--- apps/landing-pages/src/App.tsx
+++ apps/landing-pages/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { GlobalStyle } from "@arcade/library-components";
 
-import { Home } from "./pages/Home";
+import { Home } from "./pages";
 
 function App() {
 	return (
