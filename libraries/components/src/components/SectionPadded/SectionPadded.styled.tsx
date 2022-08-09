8652747e25dc70563a04fa0909977beba39903af
absolutholz <absolutholz@gmail.com>
2022-08-09 07:28:42 +0200

    Update SectionPadded component

--- libraries/components/src/components/SectionPadded/SectionPadded.styled.tsx
+++ libraries/components/src/components/SectionPadded/SectionPadded.styled.tsx
@@ -1,8 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { SpacingSize } from '../../enums';
 
-export const SectionPadded = styled.div<{ $spacing?: SpacingSize }>`
-	padding-bottom: ${({ $spacing = SpacingSize.x100 }) => $spacing};
-	padding-top: ${({ $spacing = SpacingSize.x100 }) => $spacing};
+export const SectionPadded = styled.div<{
+	$spacing?: SpacingSize;
+	$omit?: 'top' | 'bottom';
+}>`
+	${({ $omit, $spacing = SpacingSize.x100 }) =>
+		$omit !== 'bottom' &&
+		css`
+			padding-bottom: ${$spacing};
+		`}
+	${({ $omit = false, $spacing = SpacingSize.x100 }) =>
+		$omit !== 'top' &&
+		css`
+			padding-top: ${$spacing};
+		`}
 `;
