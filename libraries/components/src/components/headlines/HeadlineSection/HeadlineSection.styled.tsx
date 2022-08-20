447f264f300041104da4709f84b2658b87f4a978
absolutholz <absolutholz@gmail.com>
2022-08-20 13:30:43 -0400

    Update max line length editorconfig setting

--- libraries/components/src/components/headlines/HeadlineSection/HeadlineSection.styled.tsx
+++ libraries/components/src/components/headlines/HeadlineSection/HeadlineSection.styled.tsx
@@ -5,6 +5,5 @@ import { typoFamily, typoSize } from '../../../foundations';
 const typoSizeValues = typoSize(200);
 
 export const HeadlineSection = styled.h2`
-	font: 100 ${typoSizeValues.fontSize} / ${typoSizeValues.lineHeight}
-		${typoFamily('headline')};
+	font: 100 ${typoSizeValues.fontSize} / ${typoSizeValues.lineHeight} ${typoFamily('headline')};
 `;
--- libraries/components/src/components/headlines/HeadlineSubsection/HeadlineSubsection.styled.tsx
+++ libraries/components/src/components/headlines/HeadlineSubsection/HeadlineSubsection.styled.tsx
@@ -5,6 +5,5 @@ import { typoFamily, typoSize } from '../../../foundations';
 const typoSizeValues = typoSize(150);
 
 export const HeadlineSubsection = styled.h3`
-	font: 100 ${typoSizeValues.fontSize} / ${typoSizeValues.lineHeight}
-		${typoFamily('headline')};
+	font: 100 ${typoSizeValues.fontSize} / ${typoSizeValues.lineHeight} ${typoFamily('headline')};
 `;
