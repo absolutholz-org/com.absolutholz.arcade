7f6b80590e7f7c512962c0b974798daa2212bf55
absolutholz <absolutholz@gmail.com>
2023-05-25 03:49:42 +0200

    Update 404 page

--- libraries/components/src/components/FourOhFourPage/_FourOhFourPage.tsx
+++ libraries/components/src/components/FourOhFourPage/_FourOhFourPage.tsx
@@ -1,3 +1,22 @@
+import { PageGridContainer } from '../PageGrid';
+import { PageSection } from '../PageSection';
+import { SiteTemplate } from '../PageTemplates/SiteTemplate';
+import { Stack } from '../Stack';
+import { Typography } from '../Typography';
+
 export function FourOhFourPage(): JSX.Element {
-	return <>Well that's not right.</>;
+	return (
+		<SiteTemplate pageTitle={'Page not found'}>
+			<PageSection>
+				<PageGridContainer>
+					<Stack spaceLevelY='m'>
+						<Typography level={2}>4oh4</Typography>
+						<Typography level={0}>
+							Well that's not right.
+						</Typography>
+					</Stack>
+				</PageGridContainer>
+			</PageSection>
+		</SiteTemplate>
+	);
 }
