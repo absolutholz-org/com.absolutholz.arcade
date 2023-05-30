688c35f106ade29bba8411597035c251ee047692
absolutholz <absolutholz@gmail.com>
2023-05-30 20:39:37 +0200

    Remove unsused scavenger bingo context

--- apps/scavenger-bingo/src/context/GameConfiguration/_GameConfigurationContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-import type { IGameConfigurationContext } from './_GameConfiguration.types';
-
-const GameConfigurationContext = createContext<
-	IGameConfigurationContext | undefined
->(undefined);
-
-export default GameConfigurationContext;
