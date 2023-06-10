7b576fb845fdaf26a7e86a8bab32e57688ab180f
absolutholz <absolutholz@gmail.com>
2023-06-10 06:47:16 +0200

    Update sb home page

--- apps/scavenger-bingo/src/pages/Home/_Home.tsx
+++ apps/scavenger-bingo/src/pages/Home/_Home.tsx
@@ -1,44 +1,42 @@
+import { Link } from 'react-router-dom';
+
 import { Button } from '@arcade/library-components/src/components/Button';
 import { PageSection } from '@arcade/library-components/src/components/PageSection';
 import { PageGridContainer } from '@arcade/library-components/src/components/PageGrid/PageGridContainer';
 import { AppHomeTemplate } from '@arcade/library-components/src/components/PageTemplates/AppHomeTemplate';
-import { Stack } from '@arcade/library-components/src/components/Stack';
+import { Typography } from '@arcade/library-components/src/components/Typography';
 
 export function Home(): JSX.Element {
 	return (
 		<AppHomeTemplate appImage='green-hills.webp' appName='Scavenger Bingo'>
 			<PageSection>
 				<PageGridContainer>
+					<Typography size='xl'>
+						Grab a board, and start an exciting adventure with
+						Scavenger Bingo!
+					</Typography>
 					<p>
-						In this game, you get a special bingo board with a few
-						things you have to find. You have to look for these
-						things, and when you find them, you mark them on your
-						board. You win the game if you can mark all the things
-						in a row, column or diagonal, just like in regular
-						bingo. It's a super fun way to test your observation
-						skills and problem-solving ability while having a great
-						time with your family and friends. So, grab your board,
-						and let's start an exciting adventure with Scavenger
-						Bingo!
+						It's a super fun way to test your observation skills and
+						problem-solving ability while having a great time with
+						your family and friends.
 					</p>
+					<Button
+						size='l'
+						to='lobby'
+						variant='contained'
+						text='Play Scavenger Bingo'
+					/>
 				</PageGridContainer>
 			</PageSection>
 			<PageSection omitTopSpacing>
 				<PageGridContainer>
-					<Stack direction='row' spaceLevelY='m' spaceLevelX='m'>
-						<Button
-							size='l'
-							to='lobby'
-							variant='contained'
-							text='Play'
-						/>
-						<Button
-							size='l'
-							to='games'
-							variant='outlined'
-							text='Continue'
-						/>
-					</Stack>
+					<Typography size='l' as='h2'>
+						It looks like you have some unfinished games.
+					</Typography>
+					<p>
+						Would you like to{' '}
+						<Link to='games'>keep playing them</Link>?
+					</p>
 				</PageGridContainer>
 			</PageSection>
 		</AppHomeTemplate>
