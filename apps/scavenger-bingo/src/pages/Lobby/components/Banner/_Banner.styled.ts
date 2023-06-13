17bbc2e87c514e21cd4203e66953aa6b8ea2f026
absolutholz <absolutholz@gmail.com>
2023-06-13 11:21:28 +0200

    Update sb banner background

--- apps/scavenger-bingo/src/pages/Lobby/components/Banner/_Banner.styled.ts
+++ apps/scavenger-bingo/src/pages/Lobby/components/Banner/_Banner.styled.ts
@@ -4,10 +4,10 @@ import styled from '@emotion/styled';
 
 export const Banner = styled.div`
 	background-color: ${color('accent')};
-	background-image: linear-gradient(
+	/* background-image: linear-gradient(
 		hsl(0 0% 100% / 0.85),
 		hsl(0 0% 100% / 0.85)
-	);
+	); */
 	border: ${color('accent')} 4px solid;
 	border-radius: 10px;
 	padding: ${space('s')};
