74ea4d4f107063414c6487bc4b5de116fb8bd007
absolutholz <absolutholz@gmail.com>
2023-06-23 17:34:25 +0200

    Add sb default non-variant symbol selection

--- apps/scavenger-bingo/src/pages/Lobby/_Lobby.tsx
+++ apps/scavenger-bingo/src/pages/Lobby/_Lobby.tsx
@@ -19,10 +19,13 @@ import {
 } from './components/StickyFormFooter';
 import { Banner } from './components/Banner';
 import { ConfigProvider, useGameConfig } from './contexts/ConfigContext';
+import { useEffect } from 'react';
+
+import { default as ALL_SYMBOLS } from '../../configs/germany-road-signs/symbols.json';
 
 function _Lobby(): JSX.Element {
 	const { games, createGame } = useUnfinishedGames();
-	const { gameConfig } = useGameConfig();
+	const { gameConfig, setGameConfig } = useGameConfig();
 	const navigate = useNavigate();
 
 	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -37,6 +40,12 @@ function _Lobby(): JSX.Element {
 
 		navigate(`/game/${gameId}`);
 	}
+
+	useEffect(() => {
+		if (gameConfig.symbolIds.length === 0) {
+			setGameConfig({ symbolIds: ALL_SYMBOLS.filter((symbol) => !symbol.variant).map(({ id }) => id) });
+		}
+	}, []);
 
 	return (
 		<PageWithFooterTemplate pageTitle='Scavenger Bingo Lobby'>
