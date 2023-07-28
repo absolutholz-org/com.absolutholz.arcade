e6627b3675e6ce9c4653df94b31380e65d324579
absolutholz <absolutholz@gmail.com>
2023-07-28 21:19:39 +0200

    Add sb us road signs config

--- apps/scavenger-bingo/src/configs/index.ts
+++ apps/scavenger-bingo/src/configs/index.ts
@@ -1,10 +1,14 @@
 export const CONFIGS = [
-    {
-        id: 'germany-road-signs',
-        name: 'German Road Signs',
-    },
-    {
-        id: 'germany-license-plates',
-        name: 'German License Plates',
-    },
+	{
+		id: 'germany-road-signs',
+		name: 'German Road Signs',
+	},
+	{
+		id: 'germany-license-plates',
+		name: 'German License Plates',
+	},
+	{
+		id: 'us-road-signs',
+		name: 'US Road Signs',
+	},
 ];
