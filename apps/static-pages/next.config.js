d7396b55ce75975168ab30f106ae3416767aa3bd
absolutholz <absolutholz@gmail.com>
2023-08-06 10:20:52 +0200

    Update static pages

--- apps/static-pages/next.config.js
+++ apps/static-pages/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	transpilePackages: ['@arcade/library-components', '@arcade/library-common'],
+	pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+	distDir: 'dist',
 };
 
 module.exports = nextConfig;
